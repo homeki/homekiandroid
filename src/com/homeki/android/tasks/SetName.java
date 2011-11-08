e355a373c808349a7a2aad4f3a1db3ea3ae5644a
Linus <linus@tufve.com>
2011-11-08 21:42:00 +0100

    maybe

---
+++ src/com/homeki/android/tasks/SetName.java
@@ -0,0 +1,33 @@
+package com.homeki.android.tasks;
+
+import java.io.IOException;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.homeki.android.commands.Commands;
+
+public class SetName extends AsyncTask<Void, Void, String> {
+
+	private final Context c;
+	private int id;
+	private String name;
+	
+	public SetName(Context c, int id, String name){
+		this.c = c;
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	protected String doInBackground(Void... params) {
+		
+		String s = "";
+		try {
+			s = Commands.setName(c, id, name);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+}
