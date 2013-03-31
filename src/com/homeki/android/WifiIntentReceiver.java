b7142ab5c0cc11f858490e835281b8552205b5a6
Jonas Oscarsson <jonas.osc@rsson.nu>
2013-03-31 19:48:31 +0200

    Added user notification for failed register/unregister attempt.

--- src/com/homeki/android/WifiIntentReceiver.java
+++ src/com/homeki/android/WifiIntentReceiver.java
@@ -2,6 +2,8 @@ package com.homeki.android;
 
 import java.util.UUID;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -54,6 +56,7 @@ public class WifiIntentReceiver extends BroadcastReceiver {
 	    		}
 	    		catch (Exception e) {
 	    			Log.e(TAG, "Failed to register client to Homeki server, message: " + e.getMessage());
+	    			addNotification(context, "Failed to register client", "Homeki tried to register this device as a currently connected client after connecting to the WLAN, but failed. In future versions, it will be possible to manually retry the register attempt here.");
 	    		}
 	    	};
 	    }.start();			
@@ -75,9 +78,35 @@ public class WifiIntentReceiver extends BroadcastReceiver {
 	    		}
 	    		catch (Exception e) {
 	    			Log.e(TAG, "Failed to unregister client to Homeki server, message: " + e.getMessage());
+	    			addNotification(context, "Failed to unregister client", "Homeki tried to unregister this device as a currently connected client after leaving the WLAN, but failed. In future versions, it will be possible to manually retry the unregister attempt here.");
 	    		}
 	    	};
 	    }.start();		
+	}
+	
+	private void addNotification(Context context, String title, String text) {
+		// Prepare intent which is triggered if the
+		// notification is selected
+		/*Intent intent = new Intent(this, NotificationReceiver.class);
+		PendingIntent pIntent = PendingIntent.getActivity(context, 0, intent, 0);*/
+
+		Notification noti = new Notification.Builder(context)
+		        .setContentTitle(title)
+		        .setContentText(text)
+		        .setSmallIcon(R.drawable.ic_launcher)
+		        .getNotification();
+		        /*.setContentIntent(pIntent)
+		        .addAction(R.drawable.icon, "Call", pIntent)
+		        .addAction(R.drawable.icon, "More", pIntent)
+		        .addAction(R.drawable.icon, "And more", pIntent)*/
+
+		    
+		NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// Hide the notification after its selected
+		noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+		notificationManager.notify(0, noti); 
 	}
 	
 	public String getSsid(Context context) {
