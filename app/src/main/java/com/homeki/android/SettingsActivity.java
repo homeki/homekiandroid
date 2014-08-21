15f4a97070e0d5e61c608fdfdb91968c46b7557b
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-08-21 22:18:28 +0200

    Add actionbar and back arrow to settings

--- app/src/main/java/com/homeki/android/SettingsActivity.java
+++ app/src/main/java/com/homeki/android/SettingsActivity.java
@@ -1,12 +1,26 @@
 package com.homeki.android;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends ActionBarActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
+
+		ActionBar actionBar = getSupportActionBar();
+		actionBar.setDisplayShowCustomEnabled(true);
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (item.getItemId() != android.R.id.home) return super.onOptionsItemSelected(item);
+		finish();
+		return true;
 	}
 }
