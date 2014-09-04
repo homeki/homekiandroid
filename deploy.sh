6b9cbdf5e6c16edb179a2cfd15efa967468d53dc
Jonas Oscarsson <jonas.osc@rsson.nu>
2014-09-04 23:35:45 +0200

    Add CircleCI configuration

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This scripts extracts the keystore from environment variable, builds
+# the release APK and publishes it as a CircleCI artifact.
+
+set -e
+echo $KEYSTORE | base64 --decode > homeki.keystore
+set +e
+./gradlew assembleRelease
+rm homeki.keystore
+set -e
+cp app/build/apk/app-release.apk $CIRCLE_ARTIFACTS
---
+++ install-dependencies.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script installs the required SDK components for
+# building on CircleCI.
+
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+DEPS="$ANDROID_HOME/installed-dependencies"
+
+if [ ! -e $DEPS ]; then
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+  ln -s $ANDROID_HOME/build-tools/20.0.0/zipalign $ANDROID_HOME/tools/zipalign &&
+  echo y | android update sdk -u -a -t android-20 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+  echo y | android update sdk -u -a -t extra-android-m2repository &&
+  echo y | android update sdk -u -a -t extra-google-m2repository &&
+  touch $DEPS
+fi
